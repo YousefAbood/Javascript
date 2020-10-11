99148259916b8942a973be2224ee5b474247c1bd
YousefAbood <yalgeefary@roc-dev.com>
2020-10-11 16:58:07 +0200

    Added Title, Subtitle, 2 Images

---
+++ h02/script.js
@@ -0,0 +1,5 @@
+document.getElementById("pageTitle").innerHTML = "This is the Page Title";
+document.getElementById("subTitle").innerHTML = "This is Subtitle";
+document.getElementById("tokyoImage").src = "resources/tokyoTwo.jpg"
+document.getElementById("dubaiImage").src = "resources/dubaiOne.jpg"
+
