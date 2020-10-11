222667e94cf0c92ddaaacf53022d0171662fa0cd
YousefAbood <yalgeefary@roc-dev.com>
2020-10-11 15:53:13 +0200

    Added MijnAuto & it prints color

--- h01/Opdracht2/script.js
+++ h01/Opdracht2/script.js
@@ -3,3 +3,12 @@ var i = 0;
 for(i = 0; i <= 9; i++){
   console.log("De Waarde van i is " + i);
 }
+
+var mijnauto = {
+  merk: "Ford",
+  type: "Mondeo",
+  aantalwielen: 4,
+  kleur: "blauw"
+}
+
+console.log(mijnauto.kleur)
