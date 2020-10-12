7ddfde62c435c23b9d5fa9d23dbb45d867224067
YousefAbood <yalgeefary@roc-dev.com>
2020-10-12 17:50:27 +0200

    Added Colored Squares

--- h02/script.js
+++ h02/script.js
@@ -1,5 +1,21 @@
+// Titles
 document.getElementById("pageTitle").innerHTML = "This is the Page Title";
 document.getElementById("subTitle").innerHTML = "This is Subtitle";
-document.getElementById("tokyoImage").src = "resources/tokyoTwo.jpg"
-document.getElementById("dubaiImage").src = "resources/dubaiOne.jpg"
+document.getElementById("tokyoImage").src = "resources/tokyoTwo.jpg";
+document.getElementById("dubaiImage").src = "resources/dubaiOne.jpg";
+
+
+// Colors
+document.getElementById("squareOne").style.backgroundColor = "cyan";
+document.getElementById("squareTwo").style.backgroundColor = "green";
+document.getElementById("squareThree").style.backgroundColor = "yellow";
+document.getElementById("squareFour").style.backgroundColor = "red";
+
+document.getElementById("squareTwo").style.marginTop = "20px";
+document.getElementById("squareThree").style.marginTop = "50px";
+
+
+
+
+
 
