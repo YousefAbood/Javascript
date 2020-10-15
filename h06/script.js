1639ea0605b49209865da8fe36d640b120501ce6
YousefAbood <yalgeefary@roc-dev.com>
2020-10-15 09:46:19 +0200

    Renovated the structure of the code

--- h06/script.js
+++ h06/script.js
@@ -10,40 +10,57 @@ document.getElementById("armin").innerHTML = "Armin: " + armin;
 // Image Placement
 
 // Array of images
-var animalPictures = document.getElementsByTagName("img");
-
 var animals = ["americaneagle.jpg", "cat.jpg", "dolphin.jpg", "fox.jpg", "lion.jpg", "monkey.jpg", "panda.jpg", "penguin.jpg", "dog.jpg", "americaneagle2.jpg", "cat2.jpg", "dolphin2.jpg", "fox2.jpg", "lion2.jpg", "monkey2.jpg", "panda2.jpg", "penguin2.jpg", "dog2.jpg"];
-console.log(!("panda.jpg1" == "panda.jpg2"))
+var randomAnimals = [];
+var pictureHoldersArray = [];
 
 // Elements Creation
 images = document.getElementById("images");
-generateImages();
-
-function generateImages() {
-  
-  // Creating a Class that is the Picture Holder
-  for(var i = 0; i < 18; i++){
-    // Hide
-    hide = document.createElement("div");
-    hide.className = "hide";
-    hide.id = "hide" + i;
+generateArray();
+console.log(animals);
+console.log(randomAnimals);
 
 
-    // Picture Holder Background, Creates Picture Holders when the images are invisible.
-    pictureHolder = document.createElement("div");
-    pictureHolder.className = "pictureHolder";
-    pictureHolder.id = "pictureHolder-" + i;
-    
-    // Images Generation & Random Placement around the Images Division in HTML
-    img = document.createElement("img");
-    randomNumber = Math.floor(Math.random() * animals.length);
-    img.id = animals[randomNumber];
-    img.src = animals[randomNumber];
+// I created a randomized array that correlates directly with the pictureHolderID. 
+// For Example if index[0] of the array randomAnimals is "cat.jpg" then pictureHolder-0 is cat.jpg
+
+
+function generateArray() {
+  for(var i = 0; i < 18; i++) {
+    var randomNumber = Math.floor(Math.random() * animals.length);
+    randomAnimals.push(animals[randomNumber]);
+    randomAnimal = animals[randomNumber];
     animals.splice(randomNumber, 1);
-    // Appending the picture holder & the image
-    pictureHolder.appendChild(img);
-    images.appendChild(pictureHolder);
+
   }
 }
+
+for(var v = 0; v < 18; v++) {
+  pictureHolder = document.createElement("div");
+  pictureHolder.className = "pictureHolderClass";
+  pictureHolder.id = "" + v;
+  images.appendChild(pictureHolder);
+  pictureHolder.addEventListener("click", function(){
+      var counter = 2;
+      const currentPicture = this.id;
+      
+  });
+  
+  pictureHoldersArray[v] = pictureHolder;
+  console.log(pictureHoldersArray);
+}
+
+
+
+function showPicture(id) {
+  console.log(randomAnimals[id]);
+  pictureHoldersArray[id].style.backgroundImage = "url(" + randomAnimals[id] + ")";
+}
+
+function hidePicture(id) {
+  console.log(randomAnimals[id]);
+}
+
+
 
 
